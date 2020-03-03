5e63975308f96539ac96df3794106c7ea9aff518
Ninh Van Thang <ninhvanthang@gmail.com>
2020-03-03 13:26:15 +0700

    Update lunar_calendar.js
    update and optimize codes

--- lunar_calendar.js
+++ lunar_calendar.js
@@ -2,12 +2,12 @@
 function getMonday(d) {
   d = new Date(d);
   var day = d.getDay(),
-      diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+    diff = d.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
   return new Date(d.setDate(diff));
 }
 
-function getLday(d){
-  var day = +Intl.DateTimeFormat("zh-TW-u-ca-chinese", {day: "numeric"}).format(d).match(/\d+/)[0];
+function getLday(d) {
+  var day = +Intl.DateTimeFormat("zh-TW-u-ca-chinese", { day: "numeric" }).format(d).match(/\d+/)[0];
   return day;
 }
 
@@ -26,9 +26,8 @@ class LunarCalendar extends HTMLElement {
     //const state = hass.states[entityId];
     //const stateStr = state ? state.state : 'unavailable';
     const date = new Date();
-    const thu = ["Chủ Nhật","Thứ Hai", "Thứ Ba","Thứ Tư","Thứ Năm", "Thứ Sáu", "Thứ Bảy"];
-    const ddd =  getMonday(date);
-    
+    const ddd = getMonday(date);
+
     //ddd.getDate();
     var day = [], lday = [];
     day[0] = ddd.getDate(); lday[0] = getLday(ddd);
@@ -39,30 +38,74 @@ class LunarCalendar extends HTMLElement {
     ddd.setDate(ddd.getDate() + 1); day[5] = ddd.getDate(); lday[5] = getLday(ddd);
     ddd.setDate(ddd.getDate() + 1); day[6] = ddd.getDate(); lday[6] = getLday(ddd);
 
-    var today = '';
-    today = new Date();
-    var act = today.getDay();
-    if(act==0) act = 7;
-    today = today.getDay();
-    today = thu[today];
+    var act = date.getDay();
+    if (act == 0) act = 7; else act = act;
 
-    var currdate = (new Date()).toLocaleDateString('en-GB');
-    var y = +Intl.DateTimeFormat("zh-TW-u-ca-chinese", {
+    var today = Intl.DateTimeFormat("vi-VN", { weekday: "long" }).format(date);
+    var m='';
+    var currdate = (new Date()).toLocaleDateString('en-GB'),
+    m = +Intl.DateTimeFormat("zh-TW-u-ca-chinese", { month: "numeric" }).format(date),
+    d = +Intl.DateTimeFormat("zh-TW-u-ca-chinese", { day: "numeric" }).format(date).match(/\d+/)[0];
+
+
+    if (String(m).match(/\d+/)) {
+      var y = +Intl.DateTimeFormat("zh-TW-u-ca-chinese", {
         year: "numeric"
-    }).format(date).match(/\d+/)[0],
+      }).format(date).match(/\d+/)[0],
         m = +Intl.DateTimeFormat("zh-TW-u-ca-chinese", {
-            month: "numeric"
-        }).format(date).match(/\d+/)[0],
-        d = +Intl.DateTimeFormat("zh-TW-u-ca-chinese", {
-            day: "numeric"
+          month: "numeric"
         }).format(date).match(/\d+/)[0],
         can = ["Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ", "Canh", "Tân", "Nhâm", "Quý"],
-        chi = ["Tí", "Sửu", "Dần", "Mão", "Thìn", "Tị", "Ngọ", "Mùi", "Thân", "Dậu", "Tuất", "Hợi"];
+        chi = ["Tý", "Sửu", "Dần", "Mão", "Thìn", "Tỵ", "Ngọ", "Mùi", "Thân", "Dậu", "Tuất", "Hợi"];
 
-    y = can [(y - 1) % 10] + ' ' + chi [(y - 1) % 12];
-
-
-
+      y = can[(y - 1) % 10] + ' ' + chi[(y - 1) % 12];
+    } else {
+      var m = Intl.DateTimeFormat("zh-TW-u-ca-chinese", {month: "numeric"}).format(date).replace(/閏|月|正|二|三|四|五|六|七|八|九|十|冬|臘/gu, function (x) {
+        return {
+            閏: '*', // nhuan
+            月: '',
+            正: '1',
+            二: '2',
+            三: '3',
+            四: '4',
+            五: '5',
+            六: '6',
+            七: '7',
+            八: '8',
+            九: '9',
+            十: '10',
+            冬: '11',
+            臘: '12'
+        }[x];
+    }),
+    y = Intl.DateTimeFormat("zh-TW-u-ca-chinese", {year: "numeric"}).format(date).replace(/\d+/gu, '').replace(/年|甲|乙|丙|丁|戊|己|庚|辛|壬|癸|子|丑|寅|卯|辰|巳|午|未|申|酉|戌|亥/gu, function (x) {
+        return {
+            年: "",
+            甲: "Giáp ",
+            乙: "Ất ",
+            丙: "Bính ",
+            丁: "Đinh ",
+            戊: "Mậu ",
+            己: "Kỷ ",
+            庚: "Canh ",
+            辛: "Tân ",
+            壬: "Nhâm ",
+            癸: "Quý ",
+            子: "Tý",
+            丑: "Sửu",
+            寅: "Dần",
+            卯: "Mão",
+            辰: "Thìn",
+            巳: "Tỵ",
+            午: "Ngọ",
+            未: "Mùi",
+            申: "Thân",
+            酉: "Dậu",
+            戌: "Tuất",
+            亥: "Hợi"
+        }[x];
+    });
+    };
 
     this.content.innerHTML = `
       <div class="ldate">
@@ -183,7 +226,7 @@ class LunarCalendar extends HTMLElement {
 
   setConfig(config) {
     //if (!config.entity) {
-      //throw new Error('You need to define an entity');
+    //throw new Error('You need to define an entity');
     //}
     //this.config = config;
   }
